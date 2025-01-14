a589f66762a845e0063f2a4b894b8e2a479c7196
vizaymeena <vizaymeena@gmail.com>
2025-01-14 22:35:45 +0530

    home page for movie ticket website

--- sign_in.js
+++ sign_in.js
@@ -1,69 +1,67 @@
 /* vanta from internet source */
-  window.VANTA.BIRDS({
-    el: "#page1",
-    mouseControls: true,
-    touchControls: true,
-    gyroControls: false,
-    minHeight: 200.00,
-    minWidth: 200.00,
-    scale: 1.00,
-    scaleMobile: 1.00,
-    color1: 0x4e40a6,
-    birdSize: 0.70,
-    separation: 60.00,
-    quantity: 4.00,
-    backgroundAlpha: 0.00
-  })
+window.VANTA.BIRDS({
+ el: "#page1",
+ mouseControls: true,
+ touchControls: true,
+ gyroControls: false,
+ minHeight: 200.00,
+ minWidth: 200.00,
+ scale: 1.00,
+ scaleMobile: 1.00,
+ color1: 0x4e40a6,
+ birdSize: 0.70,
+ separation: 60.00,
+ quantity: 4.00,
+ backgroundAlpha: 0.00
+})
 
 
-  /* Js for singin page */
+  
+   let signin=()=>{
 
+   // storing input into variables 
+   let inp_email = document.querySelector("#email").value.trim();
+   let inp_password = document.querySelector("#password").value.trim();
 
+   // error variables
+   let erroremail = document.querySelector("#email");
+   let errorpassword = document.querySelector("#password");
+   
+   // stored details varibales
+   let stored_email = localStorage.getItem("email");
+   let stored_password = localStorage.getItem("password");
 
-  // Signin funciton
-  let signin=()=>{
+   
 
-    
-  // storing input into variables 
-  let inp_email = document.querySelector("#email").value.trim();
-  let inp_password = document.querySelector("#password").value.trim();
-
-
-    /* Login In Form Conditions */
-
-    /*================================================ Username Condition ============================================= */
+   /*========================================== Username Condition ============================================= */
 
    /* Empty username ondition */
-    if(inp_email=="" ){
-      let erroremail = document.querySelector("#email");
-      erroremail.placeholder= "empty username"; // showing error message into input username placeholder
-      erroremail.style.color="red";
-      erroremail.style.borderColor="red"
-      return false;                     // do not submit form if condition doesn't matches.
-    }
+  if(inp_email=="" ){
+  
+        erroremail.placeholder = "Please fill up username";       // error shows in placeholders
+        erroremail.style.borderColor="red"
+        erroremail.classList.add("input_bg_color")              // placeholder text-color
+        return false;                                          // do not submit form if condition doesn't matches.
+  }
+
+
     /* condiiton for @ and .com missing in username */
-    else if(!(inp_email.includes("@")) && !(inp_email.endswith(".com")) ){
+    else if(!inp_email.includes("@") || !inp_email.endsWith(".com") ){
 
-      let erroremail = document.querySelector("#email");
-      erroremail.placeholder = " '@ / .com' missing";
-
+          erroremail.placeholder = "Invalid email format";
+          erroremail.style.borderColor="red"
+          erroremail.classList.add("input_bg_color")
+          return false;
     }
-    
-
-
-     /* ============================================== Password Conditions ================================================ */
-
-    /* Password conditions starts */
-
-    /* When password is not a number */
-    else if(isNaN(inp_password)){
-      let errorpassword = document.querySelector("#password");
-      errorpassword.placeholder="password should be in number";
-      errorpassword.style.color="red"
-      errorpassword.style.borderColor="red"
-      return false;
-
+     
+    else if (inp_email!==stored_email){                                               // Incorrect email entered
+      erroremail.placeholder="email does not match"
+      erroremail.classList.add("input_bg_color")
     }
+
+     /* ========================================= Password Conditions =========================================== */
+
+
     /* when password does not have number and letters combo in it  */
     else if 
       (!((inp_password.match(/[1234567890]/)) && 
@@ -71,16 +69,43 @@
       (inp_password.match(/[A-Z]/)))
        )
         {
-      let errorpassword = document.querySelector("#password");
-      errorpassword.placeholder="Password should contain number and letters"
-      return false;;
+          errorpassword.placeholder="Password should be a mix up of numbers and letters";
+          errorpassword.style.borderColor="red";
+          errorpassword.style.color="red";
+          errorpassword.classList.add("input_bg_password")
+          return false;;
        }
-   
+        
+
+      /* Password length is not equal to 6 digit */
+    else if(inp_password.length<6){
+            errorpassword.placeholder="Password should be of 6 digits";
+            errorpassword.style.borderColor="red";
+            errorpassword.style.color="red";
+            errorpassword.classList.add("input_bg_password")
+            return false;;
+       }
+       else if (inp_password!==stored_password){
+        errorpassword.placeholder="Password does not match";
+        errorpassword.classList.add("input_bg_color");
+       }
+  //==================================================  P. ENDS  ===============================================//
+       
+
+  // Reset errors
+  [erroremail,errorpassword].forEach((e)=>{
+    e.placeholder="";
+    e.style.color="";
+    e.classList.remove("input_bg_password");
+  })
 
 
 
+     location.href="index.html";                // Redirecting to Home Page..
+     return true;                              // optional as it will run obivously after above condition gets true 
 
-
+  
+          
   }
 
   
--- sign_up.js
+++ sign_up.js
@@ -1,18 +1,112 @@
 
 
-// Inputs into variables
 
-let inp_username = document.querySelector("#username").value.trim();
-let inp_email = document.querySelector("#username").value.trim();
-let inp_password = document.querySelector("#username").value.trim();
-let inp_confirmpass = document.querySelector("#username").value.trim();
 
 let signup=()=>{
-                                                                   //            \\
-    localStorage.setItem("name",inp_username);                    // username set \\
-    localStorage.setItem("email",inp_email);                     //  email    set  \\
-    localStorage.setItem("password",inp_password);              //   password set   \\
-    localStorage.setItem("confirmPassword",inp_confirmpass);   //    cnfmpass set    \\
 
+    // Inputs into variables
+    let inp_username    =  document.querySelector("#username").value.trim();              //  username   \\
+    let inp_email       =  document.querySelector("#email").value.trim();                //   email       \\
+    let inp_password    =  document.querySelector("#password").value.trim();            //    password     \\
+    let inp_confirmpass =  document.querySelector("#confirm_password").value.trim();   //     confirm pass. \\
+
+    
+    // Variables for Error of inputs 
+    let err_user   = document.querySelector("#username")                                 //  username   \\
+    let err_email  = document.querySelector("#email")                                   //   email       \\
+    let err_pass   = document.querySelector("#password")                               //    password     \\
+    let err_c_pass = document.querySelector("#confirm_password")                      //     Confirm-Pass  \\
+
+
+
+
+    
+    /* Specifying rules for sign up form */
+
+    // username remained empty
+    if(inp_username == ""){      
+ 
+        err_user.placeholder       = "Username should not be empty";
+        err_user.style.borderColor = "red";
+        err_user.classList.add("textcolor") ;     // Classlist for placeholder text for all conditions
+        return false;
+    }
+
+   // Email empty
+    else if (inp_email == ""){
+      
+        err_email.placeholder       = "Empty email";
+        err_email.style.borderColor  = "red";
+        err_email.classList.add("textcolor") ;     
+        return false;
+    }
+
+    // Email not having @ and .com
+    else if(!inp_email.includes("@") || !inp_email.endsWith(".com")){
+ 
+        err_email.placeholder        = "Invalid email format";
+        err_email.style.borderColor  = "red";
+        err_email.classList.add("textcolor");
+        return false;
+    }
+
+    // Password empty and not of 6 digits
+    else if (inp_password.length < 6 || inp_password=="") {
+
+        err_pass.placeholder = "Password must be at least 6 characters";
+        err_pass.style.borderColor = "red";
+        err_pass.classList.add("textcolor");
+        return false;
+    }
+
+   
+
+    /*   
+        ^ and $ mark the start and end of the string. 
+        + means that the string can have one or more of these characters. 
+    
+    */
+    // password should be alphanumeric 
+    else if (inp_password.match(/^[a-zA-Z0-9]/+$)){    
+      
+       
+        err_pass.placeholder="password should alphanumeric";
+        err_pass.style.borderColor  = "red";
+        err_pass.classList.add("textcolor");
+        return false;
+    }
+
+   // confirm password empty
+    else if(inp_confirmpass==""){
+
+        err_c_pass.placeholder="This field cannot be remain empty";
+        err_c_pass.style.borderColor  = "red";
+        err_c_pass.classList.add("textcolor");
+        return false;
+    }
+
+     // confirm password if entered wrong
+    else if(inp_password!==inp_confirmpass){
+ 
+        err_c_pass.placeholder="password do not match"
+        err_c_pass.style.borderColor  = "red";
+        err_c_pass.classList.add("textcolor");
+        return false;
+
+    }
+
+    // resetting error styles 
+    [err_user,err_c_pass,err_email,err_pass].forEach((e)=>{
+        e.placeholder="";
+        e.style.borderColor="";
+        e.classList.remove("textcolor")
+    })
+   
+    location.href="sign_in.html"
+
+    // saving user details on browser 
+    localStorage.setItem("username",inp_username);
+    localStorage.setItem("email",inp_email);
+    localStorage.setItem("password",inp_password);
 
 }
