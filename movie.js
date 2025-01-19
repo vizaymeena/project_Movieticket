72d0f965a4a51c637fac41b4ee57687062bc6722
vizaymeena <vizaymeena@gmail.com>
2025-01-19 21:48:19 +0530

    Movies section added

---
+++ movie.js
@@ -0,0 +1,26 @@
+
+var swiper = new Swiper(".home", {
+  spaceBetween: 10,
+  centeredSlides: true,
+  // autoplay: {
+  //   delay: 2500,
+  //   disableOnInteraction: false
+  // },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true
+  },
+ 
+});
+
+// Coming soon
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+
+
