b8aa2fa9e2015b40d0593ee1b8fc4b519825ca0a
vizaymeena <vizaymeena@gmail.com>
2025-01-20 17:14:10 +0530

    update

--- movie.js
+++ movie.js
@@ -2,25 +2,30 @@
 var swiper = new Swiper(".home", {
   spaceBetween: 10,
   centeredSlides: true,
-  // autoplay: {
-  //   delay: 2500,
-  //   disableOnInteraction: false
-  // },
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false
+  },
   pagination: {
     el: ".swiper-pagination",
     clickable: true
   },
- 
 });
 
-// Coming soon
-var swiper = new Swiper(".mySwiper", {
-  slidesPerView: 3,
-  spaceBetween: 30,
+//
+// Initialize Swiper
+var swiper1 = new Swiper('.mySwiper1', {
+  slidesPerView: 2,
+  spaceBetween: 20,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
   pagination: {
-    el: ".swiper-pagination",
+    el: '.swiper-pagination',
     clickable: true,
   },
+  loop: true, // Enable infinite loop
 });
 
 
