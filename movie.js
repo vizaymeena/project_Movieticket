c48a88094ad1339579ea22617eb898b703a831ea
vizaymeena <vizaymeena@gmail.com>
2025-01-22 09:06:38 +0530

    updated

--- movie.js
+++ movie.js
@@ -1,4 +1,4 @@
-
+/* Main Swiper */
 var swiper = new Swiper(".home", {
   spaceBetween: 10,
   centeredSlides: true,
@@ -13,19 +13,57 @@ var swiper = new Swiper(".home", {
 });
 
 //
-// Initialize Swiper
+/* Anime Movies swiper */
 var swiper1 = new Swiper('.mySwiper1', {
   slidesPerView: 2,
-  spaceBetween: 20,
+  spaceBetween: 5,
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
   pagination: {
-    el: '.swiper-pagination',
+    el: '#pagination2',
     clickable: true,
   },
   loop: true, // Enable infinite loop
 });
+/* Tredning Sect Swiper */
+var progressCircle = document.querySelector(".autoplay-progress svg");
+var progressContent = document.querySelector(".autoplay-progress span");
+var swiper3 = new Swiper(".mySwiper3", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  // autoplay: {
+  //   delay: 2500,
+  //   disableOnInteraction: false
+  // },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev"
+  },
+  on: {
+    autoplayTimeLeft(s, time, progress) {
+      progressCircle.style.setProperty("--progress", 1 - progress);
+      progressContent.textContent = `${Math.ceil(time / 1000)}s`;
+    }
+  }
+});
+
+
+/* Typed Js */
+var typed1 = new Typed('#element', {
+  strings: ['<i>First</i> sentence.', '&amp; a second sentence.'],
+  typeSpeed: 50,
+});
+
+/* Heart Icon js */
+const pHeart = document.getElementById('p-heart');
+
+// Track click state
+
 
 
