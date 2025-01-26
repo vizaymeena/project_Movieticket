a464abe5123d6d3d4fd7b78f28ddb24fe8df9c2b
vizaymeena <vizaymeena@gmail.com>
2025-01-26 18:11:56 +0530

    update

--- sign_in.js
+++ sign_in.js
@@ -1,19 +1,4 @@
 /* vanta from internet source */
-window.VANTA.BIRDS({
- el: "#page1",
- mouseControls: true,
- touchControls: true,
- gyroControls: false,
- minHeight: 200.00,
- minWidth: 200.00,
- scale: 1.00,
- scaleMobile: 1.00,
- color1: 0x4e40a6,
- birdSize: 0.70,
- separation: 60.00,
- quantity: 4.00,
- backgroundAlpha: 0.00
-})
 
 
   
